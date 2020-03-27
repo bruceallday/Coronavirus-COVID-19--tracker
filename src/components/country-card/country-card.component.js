0b67c0c68cd4288cf83fd4af6ae56a7b11635eb7
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-03-27 17:39:00 -0400

    style: colors to daily totals on cards

--- src/components/country-card/country-card.component.js
+++ src/components/country-card/country-card.component.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import { textStyles } from '../../constants/textColor'
 
 import { useStyles}  from './country-card.styles'
 
 const Card = (props) => {
     const classes = useStyles()
+    const textClasses = textStyles()
 
     const { 
         country,
@@ -19,38 +21,38 @@ const Card = (props) => {
         critical } = props
     
     return(
-            <Paper elevation={3} className={classes.root}>
-            
-                <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}} >
+        <Paper elevation={3} className={classes.root}>
+        
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}} >
                 {countryCode && (
                     <img 
                         style={{ marginRight: 10 }}
                         src={`https://www.countryflags.io/${countryCode}/flat/32.png`}>
                     </img>
                 )}
-                
-                    <Typography  variant="h5">
-                        {` ${country}`}
-                    </Typography>
-                </div>
-
-
-                <Typography style={{paddingLeft: 5}} variant="body1" >
-                    {`Cases: ${cases} | Today: ${todayCases}`}
+            
+                <Typography  variant="h5">
+                    {` ${country}`}
                 </Typography>
+            </div>
 
-                <Typography style={{ paddingLeft: 5 }} variant="body1" >
-                    {`Recovered: ${recovered}`}
-                </Typography>
 
-                <Typography style={{ paddingLeft: 5 }} variant="body1" >
-                    {`Deaths: ${deaths} | Today: ${todayDeaths}`}
-                </Typography>
+            <Typography style={{paddingLeft: 5}} variant="body1" >
+                {`Cases: ${cases} | Today: `}<span className={textClasses.yellowText}>{`${todayCases}`}</span>
+            </Typography>
 
-                <Typography style={{ paddingLeft: 5 }} variant="body1" >
-                    {`Critical: ${critical}`}
-                </Typography>
-            </Paper>
+            <Typography style={{ paddingLeft: 5 }} variant="body1" >
+                {`Recovered: `}<span className={textClasses.greenText}>{`${recovered}`}</span>
+            </Typography>
+
+            <Typography style={{ paddingLeft: 5 }} variant="body1" >
+                {`Deaths: ${deaths} | Today: `}<span className={textClasses.redtext}>{`${todayDeaths}`}</span>
+            </Typography>
+
+            <Typography style={{ paddingLeft: 5 }} variant="body1" >
+                {`Critical: ${critical}`}
+            </Typography>
+        </Paper>
     )
 }
 
