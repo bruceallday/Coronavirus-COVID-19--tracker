b30c93e38dec80355297cdb3b984b8734e7d508e
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-03-27 20:25:38 -0400

    docs: updated version and README.md

--- src/components/countries/countries.component.js
+++ src/components/countries/countries.component.js
@@ -123,7 +123,7 @@ const Countries = () => {
                     <Typography className={classes.title} variant="h2" noWrap>
                         Track Coronavirus(COVID-19) 
                     </Typography>
-                    <p className={classes.version} >v1.1.3</p> 
+                    <p className={classes.version} >v1.2.0</p> 
                     <div>
                         <CssTextField
                             className={classes.textField}
