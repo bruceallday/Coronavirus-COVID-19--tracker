5b914f7d400abf0e2cac2169e395aa30d71d02c3
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-03-29 19:42:20 -0400

    fix: exit glitches on screen sizes alignment of about container

--- src/components/main-menu/main-menu.component.js
+++ src/components/main-menu/main-menu.component.js
@@ -20,29 +20,6 @@ const MainMenu = (props) => {
     } = props
 
     const classes = useStyles()
-    const [data, setData] = useState([])
-    const [isLoading, setLoading] = useState(false)
-
-    useEffect(() => {
-        getData()
-    }, [])
-
-    const getData = async () => {
-        setLoading(true)
-
-        const result = await fetch(`
-            https://corona.lmao.ninja/all
-        `)
-
-        const data = await result.json()
-
-        if (data.error) {
-            console.log("error", data.error)
-        } else {
-            setData(data)
-        }
-        setLoading(false)
-    }
 
     return (
         <div className={classes.root} >
