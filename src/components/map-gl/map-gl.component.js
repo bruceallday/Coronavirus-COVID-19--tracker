e387e99b1c92d96e1ce8d1ed918574d43ffcf16b
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-04-01 16:00:20 -0400

    feat: Tweaked data driven styling

--- src/components/map-gl/map-gl.component.js
+++ src/components/map-gl/map-gl.component.js
@@ -20,11 +20,11 @@ const ChoroplethMap = ({ covidData }) => {
     })
 
     const [viewport, setViewport] = useState({
-        latitude: 10,
-        longitude: 100,
-        zoom: 1.3,
-        bearing: 0,
-        pitch: 0
+        latitude: -15,
+        longitude: 85,
+        zoom: 1.4,
+        bearing: 20,
+        pitch: 20
     })
 
     useEffect(() => {
@@ -37,15 +37,20 @@ const ChoroplethMap = ({ covidData }) => {
            'https://bpouncey.github.io/geo-json-world/custom.geo.json',
             (error, response) => {
                 if ( !error ) {
+
                 response.features.map( country => {
                     covidData.map( countryData => {
                         if ( country.properties.iso_a3 === countryData.countryInfo.iso3 ){
+
                             country.properties = { 
                                 ...countryData, 
                                 ...country.properties
                              }
                         }
                     })
+                    if (!country.properties.cases) {
+                        country.properties.cases = 0
+                    }
                 })
                 setState({data: response})
                 } else { 
@@ -64,7 +69,6 @@ const ChoroplethMap = ({ covidData }) => {
 
     const renderTooltip = () => {
         const { hoveredFeature, x, y } = state
-
         return (
             hoveredFeature && (
                 <div className={classes.tooltip} style={{ left: x, top: y }}>
